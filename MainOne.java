c2da947128f764160d674d0e5e6122cf470a0155
Khubaib Husan <hafizmkh45@gmail.com>
2025-05-20 10:16:43 +0500

    Add files via upload

--- MainOne.java
+++ MainOne.java
@@ -3,9 +3,8 @@ package Questions.Question96;
 public class MainOne {
     public static void main(String[] args) {
         System.out.println("Days of week are: ");
-        for (enumDays wd:enumDays.values())
-        {
-            System.out.println(wd);
+        for (enumDays day : enumDays.values()) {
+            System.out.println(day.name() + " is a " + day.getTypeofDay());
         }
     }
 }
