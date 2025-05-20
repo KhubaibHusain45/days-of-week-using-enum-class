25c40059e20ce40a0902b775860599f30411a800
Khubaib Husan <hafizmkh45@gmail.com>
2025-05-20 10:07:41 +0500

    Add files via upload

---
+++ MainOne.java
@@ -0,0 +1,11 @@
+package Questions.Question96;
+
+public class MainOne {
+    public static void main(String[] args) {
+        System.out.println("Days of week are: ");
+        for (enumDays wd:enumDays.values())
+        {
+            System.out.println(wd);
+        }
+    }
+}
---
+++ enumDays.java
@@ -0,0 +1,5 @@
+package Questions.Question96;
+
+public enum enumDays {
+  MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY;
+}
